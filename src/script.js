718cacd90aab89d13a4c1d5bb8d54df5b28371cc
Moyo Oyeniyi <moyosoreoluwaoyeniyi@gmail.com>
2024-08-06 17:21:28 +0100

    updated JavaScript search

--- src/script.js
+++ src/script.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  console.log(response.data.temperature.current);
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  //make api call and update the interface
+  let apiKey = "d0f486766tb3do97a754aa844599ab3a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = searchInput.value;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
