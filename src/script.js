bd0e779df3a86508ea1db940f3317c7c0bc5f56f
Moyo Oyeniyi <moyosoreoluwaoyeniyi@gmail.com>
2024-08-06 21:50:52 +0100

    updated javascript files

--- src/script.js
+++ src/script.js
@@ -1,24 +1,36 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  console.log(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+
+  console.log(response.data.condition.description);
+
+  cityElement.innerHTML = response.data.city;
+  descriptionElement = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-  //make api call and update the interface
   let apiKey = "d0f486766tb3do97a754aa844599ab3a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
-function handleSearch(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearch);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Paris");
+
+//Dates
+let now = new Date();
+let date = now.getDate();
+let day = now.getDay();
+let hours = now.getHours();
+let minutes = now.getMinutes;
