04815c20dd716734763ea0cb4a1ccae35552f719
Moyo Oyeniyi <moyosoreoluwaoyeniyi@gmail.com>
2024-08-06 16:15:32 +0100

    Updated city to display searched element

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearch);
